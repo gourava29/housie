4cb29761741cf54e76f63d4da9322c2e4c8116fd
gourava29 <gourava29@gmail.com>
2020-04-17 17:53:31 +0530

    first commit

---
+++ precache-manifest.736a99fdfc485f97e8f88b54cdfd131e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fff5b84870f43dfcdf2804c521bd927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27821ae33d7ae64f5a6f",
+    "url": "/static/css/main.5e2f3d5d.chunk.css"
+  },
+  {
+    "revision": "a1d3f4f4a4a9e0874848",
+    "url": "/static/js/2.a51d48d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a51d48d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27821ae33d7ae64f5a6f",
+    "url": "/static/js/main.88cd07c0.chunk.js"
+  },
+  {
+    "revision": "16cc1862ebde203538d9",
+    "url": "/static/js/runtime-main.003f7c43.js"
+  },
+  {
+    "revision": "ef898acbf8bee9cd543d71dfbeebfb6a",
+    "url": "/static/media/BungeeShade-Regular.ef898acb.ttf"
+  }
+]);
